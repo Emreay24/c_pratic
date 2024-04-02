39094ff060ea8dba6b2c1845e33520454eabfcb4
Emreay24 <122545769+Emreay24@users.noreply.github.com>
2024-04-02 21:21:56 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Common;
+using System.Net.Mail;
+
+namespace degiskenler
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string name ="emre";
+            string surname ="ay";
+            string date = DateTime.Now.ToString("dd/MM/yyyy");
+            string date2 = DateTime.Now.ToString("hh.mm");
+            Console.WriteLine(name + " " + surname);
+            Console.WriteLine(date);
+            Console.WriteLine(date2);
+
+
+        }
+    }
+}
