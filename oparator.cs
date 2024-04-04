77300f0d006763374997a40d7f1867f69fd631b4
Emreay24 <122545769+Emreay24@users.noreply.github.com>
2024-04-04 18:25:02 +0300

    first commit

---
+++ oparator.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Common;
+using System.Net.Mail;
+
+namespace operatorler{
+    internal class Program{
+        private static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 30;
+        int sonuc;
+        
+        sonuc = a+b;
+        Console.WriteLine(sonuc);
+        sonuc = b-a;
+        Console.WriteLine(sonuc);
+        sonuc = a*b;
+        Console.WriteLine(sonuc);
+        sonuc =b/a;
+        Console.WriteLine(sonuc);
+        }
+    }  
+}
